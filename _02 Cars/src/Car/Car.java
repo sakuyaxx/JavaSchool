a10b3ec029218c172d7e9b282d2bd3563bfbedea
sakuyaxx <fannipal8@gmail.com>
2022-10-18 15:29:59 +0200

    02 & 03 fertig.

--- _02 Cars/src/Car/Car.java
+++ _02 Cars/src/Car/Car.java
@@ -1,8 +1,40 @@
 package Car;
 
 public class Car {
-    public String color;
+    //Gedächtnisvariablen
+    private String color;
     public Double fuelConsumption;
+    public Double fuelAmount;
     public String serialNumber;
     public String brandName;
+    public Double maxFuelAmount;
+
+    //Mehoden
+    public void drive() {
+        this.fuelAmount = this.fuelAmount - this.fuelConsumption;
+        System.out.println("I am driving");
+    }
+
+    public void brake() {
+        System.out.println("Ich bremse");
+    }
+
+    public void turboBoost() {
+        if (this.fuelAmount > (this.maxFuelAmount * 0.1)) {
+            System.out.println("SuperBoostMode activated!");
+        } else {
+            System.out.println("You do not have enough fuel to go to SuperBoostMode... :(");
+        }
+    }
+    public void honk(int amountOfRepetitions) {
+        for (int i = 0; i < amountOfRepetitions; i++){
+            System.out.println("toot");
+        }
+    }
+
+    public void getRemainingRange(){
+        //Restreichweite des Autos
+        Double remainingRange = this.fuelAmount / this.fuelConsumption;
+        System.out.println("Remaining range: " + remainingRange);
+    }
 }
--- _02 Cars/src/Car/Main.java
+++ _02 Cars/src/Car/Main.java
@@ -7,13 +7,27 @@ public class Main {
         c1.brandName = "Aston Martin";
         c1.fuelConsumption = 7.5;
         c1.serialNumber = "AM0001";
+        c1.maxFuelAmount = 120.0;
+        c1.fuelAmount = 70.0;
 
         Car c2 = new Car();
         c2.brandName = "Honda";
         c2.fuelConsumption = 3.3;
         c2.serialNumber = "H0001";
+        c2.maxFuelAmount = 150.0;
+        c2.fuelAmount = 20.0;
 
-        System.out.println(c2.serialNumber + ", " + c2.fuelConsumption);
-        System.out.println(c1.brandName);
+        /*System.out.println(c2.fuelAmount);
+        c2.drive();
+        System.out.println(c2.fuelAmount);*/
+
+        c1.brake();
+
+        c2.turboBoost();
+
+        c2.honk(13);
+
+        c1.getRemainingRange();
+
     }
 }

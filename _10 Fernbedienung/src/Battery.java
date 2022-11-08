4f1cb7afc8e92de2945b517243fd2348d93bee10
sakuyaxx <fannipal8@gmail.com>
2022-11-08 15:33:42 +0100

    10 Fernbedienung
    begonnen

---
+++ _10 Fernbedienung/src/Battery.java
@@ -0,0 +1,11 @@
+public class Battery {
+//    Eine Fernbedienung hat 2 Batterien
+//    Jede Batterie merkt sich seinen Speicherstatus
+//    Beim Aufruf der Methode getStatus() der Fernbedienung holt diese den Batteriestatus beider Batterien und berechnet den Mittelwert.
+//    Sie liefert den Ladestatus zurück!
+//
+//    Bei turnOn() werden beide Batterien um 5% geschwächt. Jede Batterie meldet sich mit "Verbraucher angeschlossen"
+//    Bei turnOff() melden beide Batterien "Kein Verbraucher angeschlossen"
+
+
+}

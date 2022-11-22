feb645d7ad09a2c76dfe365146af63d29d0d0480
sakuyaxx <fannipal8@gmail.com>
2022-11-22 10:29:42 +0100

    10 Fernbedienung
    begonnen

--- _10 Fernbedienung/src/Remote.java
+++ _10 Fernbedienung/src/Remote.java
@@ -1,2 +1,12 @@
 public class Remote {
+    //Gedächtnisvariablen
+
+    //private List<Battery> batteries;
+    public String serialNumber;
+    public String brandName;
+    public String color;
+    public String remoteSize;
+
+
+
 }
